ef6ef0dd08abbb6b07ae4999e614c3ee48498930
yaroslav-bobrovnyk <yaroslav.bobrovnyk@ix.co>
2020-07-07 17:32:57 +0300

    Additional changes commit

--- test-junit/src/test/java/SampleTest.java
+++ test-junit/src/test/java/SampleTest.java
@@ -1,4 +1,6 @@
+import config.ServerConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.After;
@@ -11,6 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SampleTest {
     protected static WebDriver driver;
     private Logger logger = LogManager.getLogger(SampleTest.class);
+    private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
 
 //    @Test
 //    public void Log(){
@@ -29,7 +32,8 @@ public class SampleTest {
     }
     @Test
     public void openPage() {
-        driver.get("https://otus.ru/");
+        driver.get(cfg.url());
+        logger.info(driver.getTitle().equals("Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям"));
         logger.info("Открыта страница отус");
     }
 
